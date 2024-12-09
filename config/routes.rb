0e0538da5729ba02e6849e107adfd3802ad4e47d
Sven de Vries <sven.devries@ijk.nl>
2024-12-09 16:00:02 +0100

    add missing comma

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require_dependency "anonymize_user_constraint"
 
 DiscoursePluginAnonymizeUser::Engine.routes.draw do
   scope path: nil, constraints: { format: %r{(json|html|\*/\*)} } do
-    resources :users id: RouteFormat.username, only: [:index], constraints: StaffConstraint.new do
+    resources :users, id: RouteFormat.username, only: [:index], constraints: StaffConstraint.new do
       put "/:id/anonymize" => "users#anonymize"
     end
   end
