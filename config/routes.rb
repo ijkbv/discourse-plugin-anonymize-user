5dd0e501370df9afcdb11b29e568d2f02949d5c4
Sven de Vries <sven.devries@ijk.nl>
2024-12-09 16:10:38 +0100

    test if build error removes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@
 require_dependency "anonymize_user_constraint"
 
 DiscoursePluginAnonymizeUser::Engine.routes.draw do
-  scope path: nil, constraints: { format: %r{(json|html|\*/\*)} } do
-    resources :users, id: RouteFormat.username, only: [:index], constraints: StaffConstraint.new do
-      put "/:id/anonymize" => "users#anonymize"
-    end
+  scope "users" do
+    put "/:id/anonymize" => "users#anonymize", :constraints => { id: /[%\w.\-]+?/ }
   end
 end
 
