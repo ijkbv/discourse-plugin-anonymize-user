9232f02b449405c62b9240649a217f74f3af75eb
Sven de Vries <sven.devries@ijk.nl>
2024-12-09 14:05:12 +0100

    test routes linting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require_dependency "anonymize_user_constraint"
+# require_dependency "anonymize_user_constraint"
 
 DiscoursePluginAnonymizeUser::Engine.routes.draw do
   scope "foo" do
@@ -7,4 +7,4 @@ DiscoursePluginAnonymizeUser::Engine.routes.draw do
   end
 end
 
-Discourse::Application.routes.draw { mount ::DiscoursePluginAnonymizeUser::Engine, at: "anonymize-plugin" }
+# Discourse::Application.routes.draw { mount ::DiscoursePluginAnonymizeUser::Engine, at: "anonymize-plugin" }
