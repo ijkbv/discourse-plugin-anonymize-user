af54cb2d057f3370f4f064a465dea3c5db64f877
Sven de Vries <sven.devries@ijk.nl>
2024-12-09 13:43:11 +0100

    fix routes linting?

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@
 require_dependency "anonymize_user_constraint"
 
 DiscoursePluginAnonymizeUser::Engine.routes.draw do
-
+  scope "users" do
+    get "/test" => "users#index"
+    get "/:id/getanonymize" => "users#getanonymize", :constraints => { id: /[\w.\-\+\%\&]+/i }
+    get "/:id/getanonymize2" => "users#getanonymize2"
+    put "/:id/anonymize" => "users#anonymize", :constraints => { id: /[\w.\-\+\%\&]+/i }
+  end
 end
 
 Discourse::Application.routes.draw { mount ::DiscoursePluginAnonymizeUser::Engine, at: "anonymize-plugin" }
