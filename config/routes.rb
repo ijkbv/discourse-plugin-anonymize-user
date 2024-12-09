720a66ca55a4a82f2992e0a0720afece7a8e9f52
Sven de Vries <sven.devries@ijk.nl>
2024-12-09 13:29:58 +0100

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 require_dependency "anonymize_user_constraint"
 
 DiscoursePluginAnonymizeUser::Engine.routes.draw do
-  resources :users, constraints: StaffConstraint.new do
+  namespace :users, constraints: StaffConstraint.new do
     get "/test" => "users#index"
     get "/:id/getanonymize" => "users#getanonymize", :constraints => { id: /[\w.\-\+\%\&]+/i }
     get "/:id/getanonymize2" => "users#getanonymize2"
