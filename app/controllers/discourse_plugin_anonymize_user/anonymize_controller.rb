9003be001d3834548cf942ba1dffdd01429c92f3
Sven de Vries <sven.devries@ijk.nl>
2024-12-10 12:51:27 +0100

    rename controller typo fix

--- app/controllers/discourse_plugin_anonymize_user/anonymize_controller.rb
+++ app/controllers/discourse_plugin_anonymize_user/anonymize_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ::DiscoursePluginAnonymizeUser
-  class AnonimizeController < ::ApplicationController
+  class AnonymizeController < ::ApplicationController
     requires_plugin PLUGIN_NAME
     requires_login
 
