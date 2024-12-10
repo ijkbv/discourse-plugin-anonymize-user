f183121aa24d3666cdf356dc94d87c86b11e09d6
Sven de Vries <sven.devries@ijk.nl>
2024-12-10 12:46:31 +0100

    rename controller

--- app/controllers/discourse_plugin_anonymize_user/anonymize_controller.rb
+++ app/controllers/discourse_plugin_anonymize_user/anonymize_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ::DiscoursePluginAnonymizeUser
-  class UsersController < ::ApplicationController
+  class AnonimizeController < ::ApplicationController
     requires_plugin PLUGIN_NAME
     requires_login
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@
 require_dependency "anonymize_user_constraint"
 
 DiscoursePluginAnonymizeUser::Engine.routes.draw do
-  namespace :users do
-    get "/" => "users#index"
-    get "/:id/anonymize" => "users#getanonymize"
-    put "/:id/anonymize" => "users#anonymize"
+  namespace :anonymize do
+    get "/" => "anonymize#index"
+    get "/:id/anonymize" => "anonymize#getanonymize"
+    put "/:id/anonymize" => "anonymize#anonymize"
   end
 end
 
