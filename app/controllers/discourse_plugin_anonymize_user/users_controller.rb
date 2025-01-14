7b9356d02c36c271f03fd405c439773bde5d1ed7
SvenIJK <159527084+SvenIJK@users.noreply.github.com>
2025-01-14 10:53:17 +0100

    Rebake posts with mentions for anonymization (#1)
    * rebake posts after anonymize
    
    * comment changes
    
    * Add sleep
    
    * Get all posts where user is mentioned instead of all posts for that user
    
    * remove empty line
    
    * remove comments
    
    * comment code for linting testing
    
    * remove @
    
    * use single quotes
    
    * reformat rebake
    
    * update to do
    
    * update where
    
    * remove where
    
    * remove rebake
    
    * minimalistic try
    
    * comment out
    
    * sleep left
    
    * post count
    
    * post.where
    
    * add find_each
    
    * do on next line
    
    * each
    
    * Get all posts where user is mentioned instead of all posts for that user

--- app/controllers/discourse_plugin_anonymize_user/users_controller.rb
+++ app/controllers/discourse_plugin_anonymize_user/users_controller.rb
@@ -18,6 +18,14 @@ module DiscoursePluginAnonymizeUser
       opts[:anonymize_ip] = "0.0.0.0"
 
       if user = UserAnonymizer.new(@user, current_user, opts).make_anonymous
+        # Sleep 1 second, otherwise the rebake is too soon
+        sleep(1)
+
+        # Rebake all posts where the user is mentioned to refresh excerpts
+        Post.where("raw LIKE ?", "%@#{@user.username}%").find_each do |post|
+          post.rebake!
+        end
+
         render json: success_json.merge(username: user.username)
       else
         render json: failed_json.merge(username: user.username)
